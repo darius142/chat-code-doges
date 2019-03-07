f6259e2cb219ed2496edc0e44b971cb686151df9
Volodymyr Padovsky <vld06@ukr.net>
2019-03-07 16:45:41 +0200

    split App into components

---
+++ src/components/Sidebar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import Divider from "@material-ui/core/Divider";
+import TextField from "@material-ui/core/TextField";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import ChatList from "./ChatList";
+import RestoreIcon from "@material-ui/icons/Restore";
+import ExploreIcon from "@material-ui/icons/Explore";
+
+const styles = theme => ({
+  drawerPaper: {
+    position: "relative",
+    height: "100%",
+    width: 320
+  },
+  drawerHeader: {
+    ...theme.mixins.toolbar,
+    paddingLeft: theme.spacing.unit * 3,
+    paddingRight: theme.spacing.unit * 3
+  }
+});
+
+const Sidebar = ({ classes, chats }) => (
+  <Drawer
+    variant="permanent"
+    classes={{
+      paper: classes.drawerPaper
+    }}
+  >
+    <div className={classes.drawerHeader}>
+      <TextField fullWidth margin="normal" placeholder="Search chats..." />
+    </div>
+    <Divider />
+    <ChatList chats={chats} />
+    <BottomNavigation showLabels>
+      <BottomNavigationAction label="My Chats" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Explore" icon={<ExploreIcon />} />
+    </BottomNavigation>
+  </Drawer>
+);
+
+export default withStyles(styles)(Sidebar);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
+import "typeface-roboto";
 
-import "./styles.css";
+ReactDOM.render(<App />, document.getElementById("root"));
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+registerServiceWorker();
